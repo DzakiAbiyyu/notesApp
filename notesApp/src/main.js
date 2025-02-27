235a3046dce23b2ce7d9fd3e103ab2342276afd8
Muhammad Dzaki Abiyyu <115612525+DzakiAbiyyu@users.noreply.github.com>
2025-02-27 11:08:37 +0700

    Delete notesApp directory

--- notesApp/src/main.js
+++
@@ -1,7 +0,0 @@
-import api from '../api/notes-api.js';
-import { renderNotes } from '../components/NoteList.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const notes = await api.getNotes();
-  renderNotes(notes);
-});
