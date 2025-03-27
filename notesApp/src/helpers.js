d03b702b2490b3679123920e7c0b2b0b6c2b0c30
Muhammad Dzaki Abiyyu <115612525+DzakiAbiyyu@users.noreply.github.com>
2025-03-27 19:28:15 +0700

    Delete notesApp directory

--- notesApp/src/helpers.js
+++
@@ -1,6 +0,0 @@
-// src/helpers
-
-export function formatDate(dateString) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-}
